d783d3489902c2af112c871f2ae0a297f5b2d080
llx123 <573381010@qq.com>
2020-02-11 20:36:13 +0800

    mvvm.js

--- js/mvvm.js
+++ js/mvvm.js
@@ -1,6 +1,22 @@
 class MVVM {
   constructor(options = {}) {
+    this.$el = options.el || document.body;
     this.$data = options.data;
-    new Compile(options.el || document.body, this)
+
+    new Observer(this.$data);
+    this.proxyData(this.$data);
+    new Compile(this.$el, this);
+  }
+  proxyData(data) {
+    Object.keys(data).forEach(key => {
+      Object.defineProperty(this, key, {
+        get() {
+          return data[key];
+        },
+        set(newVal) {
+          data[key] = newVal;
+        }
+      })
+    })
   }
 }
