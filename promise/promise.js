9d53a2a0b58dd845e1715aad55ba2a165328ebc3
llx123 <573381010@qq.com>
2020-04-05 21:57:45 +0800

    myPromise

--- promise/promise.js
+++ promise/promise.js
@@ -10,5 +10,67 @@ class MyPromise {
     this.resolveCallBacks = [];
     this.rejectCallBacks = [];
     MyPromise._this = this;
+    try {
+      fn(MyPromise.resolve, MyPromise.reject);
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+  static resolve(value) {
+    const _this = MyPromise._this;
+    // 是否是实例调用
+    const newPromise = _this instanceof MyPromise;
+    if (newPromise && _this.state === _this.states.PENDING) {
+      _this.state = _this.states.RESOLVE;
+      _this.value = value;
+      _this.resolveCallBacks.map(cb => _this.value = cb(_this.value));
+    } else if (!newPromise) {
+      const obj = new MyPromise();
+      return Object.assign(obj, {
+        state: _this.states.resolve,
+        value
+      })
+    }
+  }
+  static reject(value) {
+    const _this = MyPromise._this;
+    const newPromise = _this instanceof MyPromise;
+    if (newPromise && _this.state === _this.states.PENDING) {
+      _this.state = _this.states.RESOLVE;
+      _this.value = value;
+      _this.rejectCallBacks.map(cb => _this.value = cb(_this.value))
+    } else if (!newPromise) {
+      const obj = new MyPromise();
+      return Object.assign(obj, {
+        state: _this.states.RESOLVE,
+        value
+      })
+    }
+  }
+  then(fullfillCb, rejectCb) {
+    const {
+      PENDING,
+      RESOLVE,
+      REJECT
+    } = this.states;
+    const fCb = typeof fullfillCb === 'function' ? fullfillCb : c => c;
+    const rCb = typeof rejectCb === 'function' ? rejectCb : c => {
+      throw c;
+    }
+    switch (this.state) {
+      case PENDING:
+        this.resolveCallBacks.push(fCb);
+        this.rejectCallBacks.push(rCb);
+        break;
+      case RESOLVE:
+        this.value = fCb(this.value)
+        break;
+      case REJECT:
+        this.value = rCb(this.value);
+        break;
+      default:
+        break;
+    }
+    return this; // 链式调用
   }
 }
